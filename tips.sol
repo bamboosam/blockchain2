fa6f86e01125ed82df1afcae8b34a31f198c941b
bamboosam <bamboosaam@gmail.com>
2024-09-16 15:11:07 +0800

    project final

--- tips.sol
+++ tips.sol
@@ -1,13 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract tips{
+import "hardhat/console.sol";
 
+contract tips{
+    
     address owner;
+    uint lastTransferTimeStamp = 0;
+    uint oneYearLength = 300; // 365 days * 24 hours/day * 60 minutes/hour * 60 seconds/minute = 31,536,000 seconds
     Waitress[] waitress;
 
     constructor(){
         owner = msg.sender;
+        lastTransferTimeStamp = block.timestamp;
+        console.log(lastTransferTimeStamp);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
     }
 
     //1. put fund in smart contract
@@ -25,8 +36,13 @@ contract tips{
         uint percent;
     }
 
+    function keepAlive() public onlyOwner{
+        lastTransferTimeStamp += oneYearLength;
+        console.log(lastTransferTimeStamp);
+    }
+
     //3.2 add waitress
-    function addWaitress(address payable walletAddress,string memory name,uint percent) public{
+    function addWaitress(address payable walletAddress,string memory name,uint percent) public onlyOwner{
         bool waitressExist = false;
 
         if(waitress.length >=1){
@@ -45,7 +61,7 @@ contract tips{
 
     //4. remove user
 
-    function removePurpose(address walletAddress) public{
+    function removeWaitress(address walletAddress) public onlyOwner{
             if(waitress.length>=1){
                     
                     for(uint i=0; i<waitress.length; i++){
@@ -68,11 +84,14 @@ contract tips{
 
     //6. distribute tips
     function distributeBalance() public {
+        console.log(lastTransferTimeStamp);
+        console.log(block.timestamp);
+        require(lastTransferTimeStamp < block.timestamp, "You are not able to Claim because not the time yet!! ");
         require(address(this).balance > 0, "Insufficient balance in the contract");
         if(waitress.length>=1){
-                    
+                    uint totalamount = address(this).balance;
                     for(uint j=0; j<waitress.length; j++){
-                        uint distributeAmount = address(this).balance * waitress[j].percent / 100;
+                        uint distributeAmount = totalamount * waitress[j].percent / 100;
                         _transferFunds(waitress[j].walletAddress, distributeAmount);
                     }
         }
